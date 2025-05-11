9aebf853b63652737c684f74b70416917042e6b9
scikkk <203536673@qq.com>
2025-05-11 19:18:35 +0800

    ganvane: update prices

--- projects/ganvana/converttime.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def convert_timestamp_to_datetime(timestamp):
-    timestamp_in_seconds = timestamp / 1000.0
-    dt = datetime.datetime.fromtimestamp(timestamp_in_seconds)
-    return dt
